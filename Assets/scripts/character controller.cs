a9810c2154a599e696fd435521da4b67ebd97b42
larsenqu-afk <larsenqu@msu.edu>
2025-10-14 23:24:30 -0400

    init

---
+++ Assets/scripts/character controller.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "charactercontroller", menuName = "Scriptable Objects/charactercontroller")]
+public class charactercontroller : ScriptableObject
+{
+    
+}
